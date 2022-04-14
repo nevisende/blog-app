9c9489335127e68b35e99734773632c9d177c416
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-04-14 16:14:33 +0300

    Update: comment controller to authenticate before

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @all_users = User.all
   end
 
   def show
-    @user = current_user
+    @user = User.includes(:posts).find(params[:id])
   end
 end
