d48659cfc26aab3e37fcbae8c6e699a46894ee59
nevisende <ekonomitakipmail@gmail.com>
2022-06-21 02:55:43 +0300

    Feat: Add destroy method to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @user = User.find(params[:user_id])
   end
@@ -26,6 +28,14 @@ class PostsController < ApplicationController
         format.html { render :new }
       end
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @author = @post.author
+    @author.posts_counter -= 1
+    @post.destroy!
+    redirect_to user_posts_path(id: @author.id), notice: 'Post was deleted successfully!'
   end
 
   def post_params
