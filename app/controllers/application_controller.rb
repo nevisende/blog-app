81e30575ec2209609b9e384de03e082458300fe2
nevisende <ekonomitakipmail@gmail.com>
2022-06-23 22:45:46 +0300

    Feat: add authentication to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include JsonWebToken
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio])
     devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
+  end
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    header = header.split.last if header
+    decoded = jwt_decode(header)
+    @curr_user = User.find(decoded[:user_id])
   end
 end
