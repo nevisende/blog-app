96ae840eeae607d11480eaf2be7153362e6b39b7
vikipretium <vighneshpasupathy@gmail.com>
2022-06-24 01:57:28 +0530

    Fix authentication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   include JsonWebToken
-  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_request
 
   protected
 
